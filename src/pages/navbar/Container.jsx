01a33edb587a347f2b96886d63e3e520c56f7062
fferdiansyahh <ansyah.ferdi5@gmail.com>
2025-04-27 21:22:16 +0700

    trhird commot

--- src/pages/navbar/Container.jsx
+++ src/pages/navbar/Container.jsx
@@ -1,7 +1,5 @@
-import Footer from "./footer";
 import Footer1 from "./Footer-1";
 
-import Navbar1 from "./Navbar-1";
 import NavbarKelas from "./NavbarKelas";
 
 export default function Container({ children }) {
@@ -10,7 +8,7 @@ export default function Container({ children }) {
       <NavbarKelas />
       {children}
       {/* <Footer /> */}
-      <Footer1  dis="hidden" disp="flex"/>
+      <Footer1 dis="hidden" disp="flex" />
     </div>
   );
 }
