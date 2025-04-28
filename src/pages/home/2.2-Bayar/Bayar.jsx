e9328d10bd93c8853158fa4a9cce23a7c638dc68
fferdiansyahh <ansyah.ferdi5@gmail.com>
2025-04-28 16:03:40 +0700

    update 5

--- src/pages/home/2.2-Bayar/Bayar.jsx
+++ src/pages/home/2.2-Bayar/Bayar.jsx
@@ -6,6 +6,7 @@ import DetailProductBuy from "../1.3-Detail-Product/DetailProductBuy/DetailProdu
 import MetodeBayar from "../2.1-Metode/MetodeBayar/MetodeBayar";
 import Rincian from "../2.1-Metode/Rincian/Rincian";
 import Time from "../2.1-Metode/Time";
+import Container from "../../navbar/Container";
 
 export default function Bayar() {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 500);
@@ -23,21 +24,28 @@ export default function Bayar() {
   }, []);
   return (
     <>
-      <Time />
-      <div className="bayar-des">
-        <div className="bayar">
-          <Rincian display="flex" tit="Bayar Sekarang" link="/detail-pembayaran" to="/ubah-metode" />
-          <MetodeBayar metodee="Tata Cara Pembayaran" display="none" />
-        </div>
+      <Container>
+        <Time />
+        <div className="bayar-des">
+          <div className="bayar">
+            <Rincian
+              display="flex"
+              tit="Bayar Sekarang"
+              link="/detail-pembayaran"
+              to="/ubah-metode"
+            />
+            <MetodeBayar metodee="Tata Cara Pembayaran" display="none" />
+          </div>
 
-        <div className="bayar-des-2">
-          {isMobile ? (
-            <DetailProductBuy image={ImgBuy} display="none" dis="none" />
-          ) : (
-            <DetailProductBuy image={ImgBuy} display="block" dis="none" />
-          )}
+          <div className="bayar-des-2">
+            {isMobile ? (
+              <DetailProductBuy image={ImgBuy} display="none" dis="none" />
+            ) : (
+              <DetailProductBuy image={ImgBuy} display="block" dis="none" />
+            )}
+          </div>
         </div>
-      </div>
+      </Container>
     </>
   );
 }
