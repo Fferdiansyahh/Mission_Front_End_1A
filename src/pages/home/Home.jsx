0fdc582f9f658cba49050363bd4875b85df94bd5
fferdiansyahh <ansyah.ferdi5@gmail.com>
2025-05-12 06:51:03 +0700

    update-mission-2-11

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <Container>
-        <main className="mt-30 mb-16 mx-30  max-sm:my-10 max-sm:mx-0  border-box max-sm:mt-20">
+        <main className="my-16 mx-30  max-sm:my-10 max-sm:mx-0  border-box max-sm:mt-20">
           <Home1 />
           <Home2 />
           <Home3 />
--- src/pages/home/Home1.jsx
+++ src/pages/home/Home1.jsx
@@ -6,23 +6,28 @@ import CardImgU2 from "../../assets/u-2.png";
 import Star1 from "../../assets/star_1.png";
 import Star2 from "../../assets/star_2.png";
 import Star3 from "../../assets/star_3.png";
+import ImgBg1 from "/src/Assets/bg-1.jpeg";
 
 export default function Home1() {
   return (
     <div className="beranda-1">
-      <div className="b-1-1">
-        <h1 className="b-1-1-1">
-          Revolusi Pembelajaran: Temukan Ilmu Baru melalui Pratfrom Video
-          Interaktif!
-        </h1>
-        <p className="b-1-1-2">
-          Temukan ilmu baru yang menarik dan mendalam melalui koleksi video
-          pembelajaran berkualitas tinggi. Tidak hanya itu, Anda juga dapat
-          berpartisipasi dalam latihan interaktif yang akan meningkatkan
-          pemahaman Anda.
-        </p>
-      </div>
-      <button className="b-1-2">Temukan Video Course untuk Dipelajari!</button>
+     
+        <div className="b-1-1">
+          <h1 className="b-1-1-1">
+            Revolusi Pembelajaran: Temukan Ilmu Baru melalui Pratfrom Video
+            Interaktif!
+          </h1>
+          <p className="b-1-1-2">
+            Temukan ilmu baru yang menarik dan mendalam melalui koleksi video
+            pembelajaran berkualitas tinggi. Tidak hanya itu, Anda juga dapat
+            berpartisipasi dalam latihan interaktif yang akan meningkatkan
+            pemahaman Anda.
+          </p>
+        </div>
+        <button className="b-1-2">
+          Temukan Video Course untuk Dipelajari!
+        </button>
+     
     </div>
   );
 }
